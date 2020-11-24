87150375cbda4d5af7b023458124cb263e519a7a
Penuel Nwaneri <penuelchudi@gmail.com>
2020-11-24 14:32:09 +0100

    update app.js

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,10 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
 btn.addEventListener("click", function () {
-  // get randomNumber between 0 & 3 colors[0]
+  // get random Number between 0 - 3 colors[0]//
+  
   const randomNumber = 2;
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
-})
+});
 
