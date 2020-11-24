5dfcc22e5727e8c0b50d0da77a5a52d62e1ccfa4
Penuel Nwaneri <penuelchudi@gmail.com>
2020-11-24 14:33:18 +0100

    update app.js

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -4,7 +4,6 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   // get random Number between 0 - 3 colors[0]//
-  
   const randomNumber = 2;
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
